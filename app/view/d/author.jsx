7dbeaa7617b6b0f7c5583765187631564658fbb4
army8735 <army8735@qq.com>
2017-10-29 12:00:54 +0800

    save

--- app/view/d/author.jsx
+++ app/view/d/author.jsx
@@ -29,7 +29,7 @@ export default function(data) {
   return `<!DOCTYPE html>
 <html>
 <head>
-  ${data.helper.getDHead()}
+  ${data.helper.getDHead({ title: authorDetail.AuthorName })}
   <link rel="stylesheet" href="${data.helper.getAssetUrl('/dcommon.css')}"/>
   <link rel="stylesheet" href="${data.helper.getAssetUrl('/dauthor.css')}"/>
 </head>
