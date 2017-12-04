d5906294258c61d75f0198d996d5218a29efe460
army8735 <army8735@qq.com>
2017-12-04 10:40:20 +0800

    save

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -11,7 +11,7 @@ let helper = {
     if(url.indexOf('//') > -1) {
       return url;
     }
-    return '/public' + url + '?123';
+    return '/public' + url + '?124';
   },
   okJSON(data) {
     return {
