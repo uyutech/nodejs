60e240215ea9091b894f3d16ffe4c0ba4f115149
army8735 <army8735@qq.com>
2017-12-18 10:49:43 +0800

    save

--- app/controller/mns/mts.js
+++ app/controller/mns/mts.js
@@ -6,8 +6,9 @@ module.exports = app => {
   class Controller extends app.Controller {
     * job(ctx) {
       ctx.logger.info('job %s', '111111');
-      ctx.logger.info('head %j', ctx.head);
-      ctx.logger.info('body %j', ctx.body);
+      ctx.logger.info('heads %j', ctx.request.headers);
+      ctx.logger.info('head %j', ctx.request.header);
+      ctx.logger.info('body %j', ctx.request.body);
       ctx.status = 204;
     }
   }
