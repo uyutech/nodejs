07d589fc85f005430570e8d39f9aa0feceff2be4
army8735 <army8735@qq.com>
2017-12-23 18:27:51 +0800

    save

--- app/controller/h5/version.js
+++ app/controller/h5/version.js
@@ -9,8 +9,8 @@ module.exports = app => {
     * index(ctx) {
       ctx.body = {
         success: true,
-        version: 26,
-        url: 'http://circling.net.cn/h5/h5-0.3.5.zip',
+        version: 27,
+        url: 'http://circling.net.cn/h5/h5-0.3.6.zip',
       };
     }
   }
