a15a9802207e61d4ced25c7a20ccf85f23011c37
army8735 <army8735@qq.com>
2018-04-20 11:40:33 +0800

    save

--- app/controller/h52/circle.js
+++ app/controller/h52/circle.js
@@ -42,7 +42,7 @@ class Controller extends egg.Controller {
       return;
     }
     let offset = parseInt(body.offset) || 0;
-    let res = await service.circle.postList(circleId, uid, offset, LIMIT);
+    let res = await service.circle.postList(id, uid, offset, LIMIT);
     res.limit = LIMIT;
     ctx.body = ctx.helper.okJSON(res);
   }
