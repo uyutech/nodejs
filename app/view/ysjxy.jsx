e852c66857cb3c8b0bb9a0f3a633be183f764cb9
army8735 <army8735@qq.com>
2018-07-02 09:46:42 +0800

    头部使用异世谣

--- app/view/ysjxy.jsx
+++ app/view/ysjxy.jsx
@@ -20,7 +20,18 @@ export default function(data) {
   return `<!DOCTYPE html>
 <html>
 <head>
-  ${data.helper.getDHead({ title: info.title })}
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta charset="utf-8"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <meta name="renderer" content="webkit"/>
+  <meta name="apple-mobile-web-app-capable" content="yes"/>
+  <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+  <meta name="format-detection" content="telephone=no"/>
+  <meta name="format-detection" content="email=no"/>
+  <meta name="wap-font-scale" content="no"/>
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
+  <title>异世交响月</title>
+  <link rel="icon" href="//zhuanquan.xin/img/ba2257a30816928629e0b47f9e6f7b38.png" type="image/x-icon">
   <link rel="stylesheet" href="${data.helper.getAssetUrl('/rcommon.css')}"/>
   <link rel="stylesheet" href="${data.helper.getAssetUrl('/ysjxy.css')}"/>
 </head>
