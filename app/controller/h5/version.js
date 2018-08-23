b55c57378cf9e8efa791d3833064dd6888bc5399
army8735 <army8735@qq.com>
2018-08-23 20:20:40 +0800

    version

--- app/controller/h5/version.js
+++ app/controller/h5/version.js
@@ -16,11 +16,11 @@ class Controller extends egg.Controller {
       app.redis.get(urlCacheKey)
     ]);
     if(!version) {
-      version = 197;
+      version = 198;
       await app.redis.set(versionCacheKey, version);
     }
     if(!url) {
-      url = 'https://circling.net.cn/h5/h5-0.7.67.zip';
+      url = 'https://circling.net.cn/h5/h5-0.7.68.zip';
       await app.redis.set(urlCacheKey, url);
     }
     ctx.body = {
